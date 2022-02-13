7898fe239b7b4637682b18166824a0de040134b7
John Ryan <jtigger@infosysengr.com>
2022-02-12 20:08:32 -0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <adder.h>
+
+int main()
+{ 
+  std::cout << "Hello World (from C++)" << std::endl;
+
+  std::cout << "sum = " << add(72.1, 73.8) << std::endl;
+  return 0;
+}
